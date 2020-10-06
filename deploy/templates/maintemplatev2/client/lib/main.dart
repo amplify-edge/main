bbba331a84770b35272579966bca578eecdb2391
gutterbacon <gutterbacon@protonmail.com>
2020-10-06 14:57:10 +0000

    maintemplatev2: main.go uses sys and sys-share

--- deploy/templates/maintemplatev2/client/lib/main.dart
+++ deploy/templates/maintemplatev2/client/lib/main.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:maintemplate/app_module.dart';
-import 'package:mod_ion/mod_ion.dart';
-import 'package:mod_kanban/mod_kanban.dart';
+// import 'package:mod_ion/mod_ion.dart';
+// import 'package:mod_kanban/mod_kanban.dart';
 import 'package:mod_main/core/i18n/mod_main_localization.dart';
-import 'package:mod_account/core/i18n/mod_account_localization.dart';
-import 'package:mod_chat/core/i18n/mod_chat_localization.dart';
-import 'package:mod_write/core/i18n/mod_write_localization.dart';
+// import 'package:mod_account/core/i18n/mod_account_localization.dart';
+// import 'package:mod_chat/core/i18n/mod_chat_localization.dart';
+// import 'package:mod_write/core/i18n/mod_write_localization.dart';
 import 'package:provider/provider.dart' as provider;
 import 'package:sys_core/sys_core.dart';
 
@@ -15,7 +15,7 @@ import '././core/core.dart';
 import 'modules/settings/settings.dart';
 
 // Bottom Up approach .....
-import 'package:mod_geo/core/core.dart';
+// import 'package:mod_geo/core/core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -63,13 +63,13 @@ class _AppState extends State<App> {
       navigatorKey: Modular.navigatorKey,
       localizationsDelegates: [
         AppLocalizationsDelegate(model.locale), //maintemplate delegate
-        ModGeoAppLocalizationsDelegate(model.locale),
-        ModAccountLocalizationsDelegate(model.locale),
+        // ModGeoAppLocalizationsDelegate(model.locale),
+        // ModAccountLocalizationsDelegate(model.locale),
         ModMainLocalizationsDelegate(model.locale),
-        ModChatLocalizationsDelegate(model.locale),
-        ModWriteLocalizationsDelegate(model.locale),
-        ModIonLocalizationsDelegate(model.locale),
-        ModKanbanLocalizationsDelegate(model.locale),
+        // ModChatLocalizationsDelegate(model.locale),
+        // ModWriteLocalizationsDelegate(model.locale),
+        // ModIonLocalizationsDelegate(model.locale),
+        // ModKanbanLocalizationsDelegate(model.locale),
         GlobalWidgetsLocalizations.delegate,
         GlobalMaterialLocalizations.delegate
       ],
