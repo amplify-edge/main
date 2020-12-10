b1bc72c55a3b88983f16ad5b6b3bebdc937b9d0c
gutterbacon <gutterbacon@protonmail.com>
2020-12-10 07:13:02 +0000

    embedding version.json, also progress thus far on org-y experiment

---
+++ deploy/projects/org-y/flutter/build/build/web/main.dart.js_5.part.js
@@ -0,0 +1,42 @@
+self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
+$__dart_deferred_initializers__.current=function(a,b,c,$){var C,H,J,P,W,R,L,G,M,B,U,A={
+ayU:function(d){var x="Configuraciones"
+return P.j(["changeLanguageSet",Z.ay("Cambiar idioma"),"changeThemeSet",Z.ay("Cambiar de tema"),"tabIon",Z.ay("Ion"),"tabKanban",Z.ay("Kanban"),"tabSettings",Z.ay(x),"tabadmin",Z.ay("Administraci\xf3n"),"tabchat",Z.ay("Charla"),"tabhome",Z.ay("Hogar"),"tabmap",Z.ay("Mapa"),"tabsettings",Z.ay(x),"tabsurvey",Z.ay("Encuesta"),"tabwriter",Z.ay("Escritor")],y.e,y.c)},
+Mw:function Mw(d){this.a=d}},V,E,O,T,F,Z,N,Q,Y,K,X,S,D
+a.setFunctionNamesIfNecessary([A])
+C=c[0]
+H=c[1]
+J=c[2]
+P=c[3]
+W=c[4]
+R=c[5]
+L=c[6]
+G=c[7]
+M=c[8]
+B=c[9]
+U=c[10]
+A=a.updateHolder(c[11],A)
+V=c[12]
+E=c[13]
+O=c[14]
+T=c[15]
+F=c[16]
+Z=c[17]
+N=c[18]
+Q=c[19]
+Y=c[20]
+K=c[21]
+X=c[22]
+S=c[23]
+D=c[24]
+A.Mw.prototype={
+gmJ:function(){return"es"},
+glt:function(){return this.a}}
+var z=a.updateTypes(["@(@)"]);(function installTearOffs(){var x=a._static_1
+x(A,"aWJ","ayU",0)})();(function inheritance(){var x=a.inherit
+x(A.Mw,Z.eq)})()
+H.op(b.typeUniverse,JSON.parse('{"oB":"a3","pq":"a3","oA":"ah","pG":"ah","rQ":"dT","oK":"al","qU":"aa","pk":"aa","pK":"eM","rw":"dv","p9":"f_","ph":"eE","p1":"cT","rc":"cT","pL":"fo","pe":"bN","oH":"fz","Mw":{"eq":[]}}'))
+0
+var y={c:H.T("eB*"),e:H.T("i*")};(function lazyInitializers(){var x=a.lazyOld
+x($,"b3I","aJv",function(){return new A.Mw(A.ayU(A.aWJ()))})})()}
+$__dart_deferred_initializers__["nMExSY6jUj2Le6/3Sf4U7mas6K4="] = $__dart_deferred_initializers__.current
---
+++ deploy/projects/org-y/flutter/build/build/web/main.dart.js_6.part.js
@@ -0,0 +1,42 @@
+self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
+$__dart_deferred_initializers__.current=function(a,b,c,$){var C,H,J,P,W,R,L,G,M,B,U,A,V,E,O={
+ayT:function(d){var x="R\xe9glages"
+return P.j(["changeLanguageSet",Z.ay("Changer de langue"),"changeThemeSet",Z.ay("Change le th\xe8me"),"tabIon",Z.ay("Ion"),"tabKanban",Z.ay("Kanban"),"tabSettings",Z.ay(x),"tabadmin",Z.ay("Administrateur"),"tabchat",Z.ay("Bavarder"),"tabhome",Z.ay("Accueil"),"tabmap",Z.ay("Carte"),"tabsettings",Z.ay(x),"tabsurvey",Z.ay("Sondage"),"tabwriter",Z.ay("\xc9crivain")],y.e,y.c)},
+Mv:function Mv(d){this.a=d}},T,F,Z,N,Q,Y,K,X,S,D
+a.setFunctionNamesIfNecessary([O])
+C=c[0]
+H=c[1]
+J=c[2]
+P=c[3]
+W=c[4]
+R=c[5]
+L=c[6]
+G=c[7]
+M=c[8]
+B=c[9]
+U=c[10]
+A=c[11]
+V=c[12]
+E=c[13]
+O=a.updateHolder(c[14],O)
+T=c[15]
+F=c[16]
+Z=c[17]
+N=c[18]
+Q=c[19]
+Y=c[20]
+K=c[21]
+X=c[22]
+S=c[23]
+D=c[24]
+O.Mv.prototype={
+gmJ:function(){return"fr"},
+glt:function(){return this.a}}
+var z=a.updateTypes(["@(@)"]);(function installTearOffs(){var x=a._static_1
+x(O,"aWK","ayT",0)})();(function inheritance(){var x=a.inherit
+x(O.Mv,Z.eq)})()
+H.op(b.typeUniverse,JSON.parse('{"oB":"a3","pq":"a3","oA":"ah","pG":"ah","rQ":"dT","oK":"al","qU":"aa","pk":"aa","pK":"eM","rw":"dv","p9":"f_","ph":"eE","p1":"cT","rc":"cT","pL":"fo","pe":"bN","oH":"fz","Mv":{"eq":[]}}'))
+0
+var y={c:H.T("eB*"),e:H.T("i*")};(function lazyInitializers(){var x=a.lazyOld
+x($,"b3J","aJw",function(){return new O.Mv(O.ayT(O.aWK()))})})()}
+$__dart_deferred_initializers__["FQ1ThPmDaTt2Clv55OCoBn0CiSs="] = $__dart_deferred_initializers__.current
