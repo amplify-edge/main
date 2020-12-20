d287c19a3d9395faa332215ce08cf97d6b3cb95c
gutterbacon <gutterbacon@protonmail.com>
2020-12-20 22:43:45 +0700

    include generated messages

---
+++ deploy/templates/maintemplatev2/client/lib/core/i18n/generated/messages_es.dart
@@ -0,0 +1,37 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a es locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'es';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static _notInlinedMessages(_) => <String, Function> {
+    "changeLanguageSet" : MessageLookupByLibrary.simpleMessage("Cambiar idioma"),
+    "changeThemeSet" : MessageLookupByLibrary.simpleMessage("Cambiar de tema"),
+    "tabIon" : MessageLookupByLibrary.simpleMessage("Ion"),
+    "tabKanban" : MessageLookupByLibrary.simpleMessage("Kanban"),
+    "tabSettings" : MessageLookupByLibrary.simpleMessage("Configuraciones"),
+    "tabadmin" : MessageLookupByLibrary.simpleMessage("Administración"),
+    "tabchat" : MessageLookupByLibrary.simpleMessage("Charla"),
+    "tabhome" : MessageLookupByLibrary.simpleMessage("Casa"),
+    "tabmap" : MessageLookupByLibrary.simpleMessage("Mapa"),
+    "tabsettings" : MessageLookupByLibrary.simpleMessage("Configuraciones"),
+    "tabsurvey" : MessageLookupByLibrary.simpleMessage("Encuesta"),
+    "tabwriter" : MessageLookupByLibrary.simpleMessage("Escritor")
+  };
+}
---
+++ deploy/templates/maintemplatev2/client/lib/core/i18n/generated/messages_messages.dart
@@ -0,0 +1,37 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a messages locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'messages';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static _notInlinedMessages(_) => <String, Function> {
+    "changeLanguageSet" : MessageLookupByLibrary.simpleMessage("Change Language"),
+    "changeThemeSet" : MessageLookupByLibrary.simpleMessage("Change Theme"),
+    "tabIon" : MessageLookupByLibrary.simpleMessage("Ion"),
+    "tabKanban" : MessageLookupByLibrary.simpleMessage("Kanban"),
+    "tabSettings" : MessageLookupByLibrary.simpleMessage("Settings"),
+    "tabadmin" : MessageLookupByLibrary.simpleMessage("Admin"),
+    "tabchat" : MessageLookupByLibrary.simpleMessage("Chat"),
+    "tabhome" : MessageLookupByLibrary.simpleMessage("Home"),
+    "tabmap" : MessageLookupByLibrary.simpleMessage("Map"),
+    "tabsettings" : MessageLookupByLibrary.simpleMessage("Settings"),
+    "tabsurvey" : MessageLookupByLibrary.simpleMessage("Survey"),
+    "tabwriter" : MessageLookupByLibrary.simpleMessage("Writer")
+  };
+}
