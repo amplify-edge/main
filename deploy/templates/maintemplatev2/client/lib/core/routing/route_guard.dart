5fda021aecbc366fe82c36ceb5dd2508b6e130f8
gutterbacon <gutterbacon@protonmail.com>
2021-01-24 15:37:19 +0000

    modify route guard

--- deploy/templates/maintemplatev2/client/lib/core/routing/route_guard.dart
+++ deploy/templates/maintemplatev2/client/lib/core/routing/route_guard.dart
@@ -2,13 +2,11 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:sys_share_sys_account_service/view/widgets/view_model/auth_nav_view_model.dart';
 
 class NavRailGuard implements RouteGuard {
-  final authNavVm = Modular.get<AuthNavViewModel>();
-
   @override
   Future<bool> canActivate(String path, ModularRoute router) async {
-    final navIdx = authNavVm.getDynamicNavIndex(path);
+    final navIdx = Modular.get<AuthNavViewModel>().getDynamicNavIndex(path);
     if (navIdx > 0) {
-      authNavVm.setCurrentNavIndex(navIdx);
+      Modular.get<AuthNavViewModel>().setCurrentNavIndex(navIdx);
     }
     return true;
   }
