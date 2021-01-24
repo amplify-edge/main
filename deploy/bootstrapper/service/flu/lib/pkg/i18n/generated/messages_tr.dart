8373027904c0a0289ce8d2a9578a116f7c0f1f41
gutterbacon <gutterbacon@protonmail.com>
2021-01-25 01:56:25 +0700

    add turkish

---
+++ deploy/bootstrapper/service/flu/lib/pkg/i18n/generated/messages_tr.dart
@@ -0,0 +1,39 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a tr locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'tr';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static _notInlinedMessages(_) => <String, Function> {
+    "allOfYourDataWillBeDeleted" : MessageLookupByLibrary.simpleMessage("Tüm verileriniz silinecek!"),
+    "bootstrapDetails" : MessageLookupByLibrary.simpleMessage("Bootstrap Ayrıntıları"),
+    "bootstrapFileWillBeDeleted" : MessageLookupByLibrary.simpleMessage("Bootstrap dosyası silinecek!"),
+    "cancel" : MessageLookupByLibrary.simpleMessage("İptal etmek"),
+    "confirm" : MessageLookupByLibrary.simpleMessage("Onaylamak"),
+    "delete" : MessageLookupByLibrary.simpleMessage("Sil"),
+    "deleteBootstrap" : MessageLookupByLibrary.simpleMessage("Bootstrap\'i sil"),
+    "editBootstrap" : MessageLookupByLibrary.simpleMessage("Bootstrap\'i Düzenle"),
+    "execute" : MessageLookupByLibrary.simpleMessage("Yürüt"),
+    "executeBootstrap" : MessageLookupByLibrary.simpleMessage("Bootstrap\'i çalıştırın"),
+    "listBootstrap" : MessageLookupByLibrary.simpleMessage("Bootstrap\'i listeleyin"),
+    "noBootstraps" : MessageLookupByLibrary.simpleMessage("Önyükleme yok"),
+    "pickImageAndCopyTheResultingValue" : MessageLookupByLibrary.simpleMessage("Resmi seçin ve elde edilen değeri kopyalayın"),
+    "selectBootstrap" : MessageLookupByLibrary.simpleMessage("Bootstrap\'i seçin")
+  };
+}
