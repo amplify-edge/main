4321b1c425bc2d5dd7bd3aa26eeeee1814c2b4a2
gutterbacon <gutterbacon@protonmail.com>
2021-01-26 23:55:18 +0700

    some update

---
+++ deploy/templates/maintemplatev2/client/lib/core/i18n/generated/messages_tr.dart
@@ -0,0 +1,39 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a tr locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'tr';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static _notInlinedMessages(_) => <String, Function> {
+    "changeLanguageSet" : MessageLookupByLibrary.simpleMessage("Dili değiştir"),
+    "changeThemeSet" : MessageLookupByLibrary.simpleMessage("Temayı değiştir"),
+    "noMenuSelected" : MessageLookupByLibrary.simpleMessage("Menü seçilmedi, lütfen navigasyon rayından herhangi birine tıklayın"),
+    "tabIon" : MessageLookupByLibrary.simpleMessage("İyon"),
+    "tabKanban" : MessageLookupByLibrary.simpleMessage("Kanban"),
+    "tabSettings" : MessageLookupByLibrary.simpleMessage("Ayarlar"),
+    "tabSuper" : MessageLookupByLibrary.simpleMessage("Süper Yönetici"),
+    "tabadmin" : MessageLookupByLibrary.simpleMessage("Yönetici"),
+    "tabchat" : MessageLookupByLibrary.simpleMessage("Sohbet"),
+    "tabhome" : MessageLookupByLibrary.simpleMessage("Ev"),
+    "tabmap" : MessageLookupByLibrary.simpleMessage("Harita"),
+    "tabsettings" : MessageLookupByLibrary.simpleMessage("Ayarlar"),
+    "tabsurvey" : MessageLookupByLibrary.simpleMessage("Anket"),
+    "tabwriter" : MessageLookupByLibrary.simpleMessage("yazar")
+  };
+}
